697c3834277101e9aa2b26dfae4537f893aa4e74
perchbird <187565992+perchbirdd@users.noreply.github.com>
2025-06-29 12:01:55 -0400

    Add tests

---
+++ Chronofoil.Web.Tests/InfoTests.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel;
+using Chronofoil.Common;
+using Chronofoil.Common.Info;
+using Shouldly;
+using Xunit;
+
+namespace Chronofoil.Web.Tests;
+
+[Trait("Category", "UnitTest")]
+public class InfoTests(ApiTestFixture fixture) : IClassFixture<ApiTestFixture>
+{
+    [Fact]
+    public async Task Info_Tos()
+    {
+        var expected = new TosResponse { Version = 1, Text = "Test Terms of Service" };
+        fixture.InfoServiceMock
+            .Setup(s => s.GetCurrentTos())
+            .Returns(ApiResult<TosResponse>.Success(expected));
+
+        var result = await fixture.ApiClient.GetTos();
+
+        result.ShouldNotBeNull();
+        result.StatusCode.ShouldBe(ApiStatusCode.Success);
+        result.Data.Version.ShouldBe(expected.Version);
+        result.Data.Text.ShouldBe(expected.Text);
+    }
+
+    [Fact]
+    public async Task Info_Faq()
+    {
+        var expected = new FaqResponse { Entries = [new FaqEntry { Question = "Q", Answer = "A" }] };
+        fixture.InfoServiceMock
+            .Setup(s => s.GetCurrentFaq())
+            .Returns(ApiResult<FaqResponse>.Success(expected));
+
+        var result = await fixture.ApiClient.GetFaq();
+
+        result.ShouldNotBeNull();
+        result.StatusCode.ShouldBe(ApiStatusCode.Success);
+        result.Data.Entries.ShouldBeEquivalentTo(expected.Entries);
+    }
+}
---
+++ Chronofoil.Web.Tests/TestHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using Chronofoil.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chronofoil.Web.Tests;
+
+public static class TestHelpers
+{
+    public static ApiResult ToApiResult(this ActionResult<ApiResult> value)
+    {
+        var result = (value.Result as ObjectResult)?.Value as ApiResult;
+        return result!;
+    }
+
+    public static ApiResult<T> ToApiResult<T>(this ActionResult<ApiResult<T>> value) where T : class
+    {
+        var result = (value.Result as ObjectResult)?.Value as ApiResult<T>;
+        return result!;
+    }
+}
